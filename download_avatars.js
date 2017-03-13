46a2bfb46183bc76faf51fa787cdf7d9b633831b
sadooghi <gh.sadooghi@gmail.com>
2017-03-13 22:51:12 +0000

    Make the request

--- download_avatars.js
+++ download_avatars.js
@@ -6,12 +6,24 @@ var GITHUB_USER = "sadooghi";
 var GITHUB_TOKEN = "affffe5f88f4a2843caff25b4536380214d68914";
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
-  // ...
+  var options = {
+    url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
+  }
+  // console.log(requestURL);
+
+  request(options, function(err, response) {
+  if (err) throw err;
+  console.log('Response:', response.body);
+
+  });
+
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
