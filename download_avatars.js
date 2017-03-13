17f7675da98535db1c94f867f78f659a36bf94be
sadooghi <gh.sadooghi@gmail.com>
2017-03-13 22:03:26 +0000

    add requestURL

--- download_avatars.js
+++ download_avatars.js
@@ -2,7 +2,12 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var GITHUB_USER = "sadooghi";
+var GITHUB_TOKEN = "affffe5f88f4a2843caff25b4536380214d68914";
+
 function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
   // ...
 }
 
