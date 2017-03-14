345bd5396d3e637f85026ca40746d991cfb1956f
sadooghi <gh.sadooghi@gmail.com>
2017-03-14 00:27:01 +0000

    finished

--- download_avatars.js
+++ download_avatars.js
@@ -2,41 +2,48 @@ var request = require('request');
 var fs = require('fs');
 var args = process.argv;
 
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 var GITHUB_USER = "sadooghi";
 var GITHUB_TOKEN = "affffe5f88f4a2843caff25b4536380214d68914";
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
-    url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
-    headers: {
-      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+if (process.argv.length >= 4) {
+  function getRepoContributors(repoOwner, repoName, cb) {
+    var options = {
+      url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+      headers: {
+        'User-Agent': 'GitHub Avatar Downloader - Student Project'
+      }
     }
+
+    request(options, function(err, response) {
+      if (err) throw err;
+
+      cb(err, JSON.parse(response.body));
+    });
   }
 
-  request(options, function(err, response) {
-    if (err) throw err;
 
-    cb(err, JSON.parse(response.body));
+
+  function downloadImageByURL(url, filePath) {
+    request(url, function(err, response) {
+      if (err) throw err;
+      request.get(url).pipe(fs.createWriteStream(filePath));
+    });
+  }
+
+  getRepoContributors(process.argv[2], process.argv[3], function(err, result) {
+    console.log("Errors:", err);
+    console.log("Result:", result);
+    for (var i = 0; i < result.length; i++){
+      console.log(result[i].avatar_url);
+      downloadImageByURL(result[i].avatar_url, "avatar/" + result[i].login + ".jpg")
+    }
   });
+} else {
+  console.log("you should inter 2 inputs for repo owner and repo name!");
 }
 
 
-
-function downloadImageByURL(url, filePath) {
-  request(url, function(err, response) {
-    if (err) throw err;
-    request.get(url).pipe(fs.createWriteStream(filePath));
-  });
-}
-
-getRepoContributors(process.argv[2], process.argv[3], function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-  for (var i = 0; i < result.length; i++){
-    console.log(result[i].avatar_url);
-    downloadImageByURL(result[i].avatar_url, "avatar/" + result[i].login + ".jpg")
-  }
-});
 
